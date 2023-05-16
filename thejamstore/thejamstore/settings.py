981471b6df3321ef1029d6f02cfcd64ef4fb06d2
javierlnmn <jlm03123@gmail.com>
2023-05-16 09:53:52 +0200

    Remodelada interfaz de panel de administrador

--- thejamstore/thejamstore/settings.py
+++ thejamstore/thejamstore/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "jet.dashboard",
+    "jet",
     "general.apps.GeneralConfig",
     "productos.apps.ProductosConfig",
     "usuarios.apps.UsuariosConfig",
@@ -144,3 +146,37 @@ LOGOUT_REDIRECT_URL = "inicio"
 # Media 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+
+# Jet Admin Interface
+JET_THEMES = [
+    {
+        'theme': 'default', # theme folder name
+        'color': '#47bac1', # color of the theme's button in user menu
+        'title': 'Default' # theme title
+    },
+    {
+        'theme': 'green',
+        'color': '#44b78b',
+        'title': 'Green'
+    },
+    {
+        'theme': 'light-green',
+        'color': '#2faa60',
+        'title': 'Light Green'
+    },
+    {
+        'theme': 'light-violet',
+        'color': '#a464c4',
+        'title': 'Light Violet'
+    },
+    {
+        'theme': 'light-blue',
+        'color': '#5EADDE',
+        'title': 'Light Blue'
+    },
+    {
+        'theme': 'light-gray',
+        'color': '#222',
+        'title': 'Light Gray'
+    }
+]
