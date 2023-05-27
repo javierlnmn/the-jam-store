79f9443412190aa03807b79bb08b59c292932f98
andreidanielpopa <andreidanielpopa02@gmail.com>
2023-05-27 19:17:13 +0200

    Funcion indice

--- thejamstore/general/urls.py
+++ thejamstore/general/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
-from .views import *
+from .views import indice
 
 urlpatterns = [
-    path('', IndiceView.as_view(), name='indice'),
+    path('', indice, name='indice'),
 ]
