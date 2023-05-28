dd56c312a580b4fabe2ff28ba52304df19911eff
andreidanielpopa <andreidanielpopa02@gmail.com>
2023-05-28 11:38:45 +0200

    Funcionalidad productos recomendados

--- thejamstore/productos/views.py
+++ thejamstore/productos/views.py
@@ -7,10 +7,13 @@ directorio_templates = "productos/"
 
 def producto_detalle(request, id_producto):
     
+    productos_recomendados = Producto.objects.order_by('?')[:4]
+    
     producto_detalle = get_object_or_404(Producto, pk=id_producto)
 
     contexto = {
         "producto_detalle": producto_detalle,
+        "productos_recomendados":productos_recomendados,
     }
     
     print(producto_detalle.producto_color.all())
