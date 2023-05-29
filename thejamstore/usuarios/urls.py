0cb5e73b8202f93e864d6bc29b69885d33239614
javierlnmn <jlm03123@gmail.com>
2023-05-29 21:06:48 +0200

    Correccion nombre URL

--- thejamstore/usuarios/urls.py
+++ thejamstore/usuarios/urls.py
@@ -6,5 +6,5 @@ app_name = 'usuarios'
 urlpatterns = [
     path('iniciar-sesion/', iniciar_sesion, name='iniciar_sesion'),
     path('cerrar-sesion/', cerrar_sesion, name='cerrar_sesion'),
-    path('registrar_usuario/', registrar_usuario, name='registro'),
+    path('registro/', registrar_usuario, name='registrar_usuario'),
 ]
